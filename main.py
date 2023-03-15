b3f64811c3c0ca0c330c315bc8841da280b4810e
Aswa Atallah Anam <117686611+IncognitoPierce@users.noreply.github.com>
2023-03-15 08:25:33 +0700

    Update H.265 -> H.266
    H.266/VVC (Versatile Video Coding) atau yang dikenal juga sebagai Future Video Coding (FVC) adalah format video terbaru yang lebih baik daripada H.265/HEVC. H.266/VVC dapat memberikan kompresi video hingga 50% lebih baik dari H.265/HEVC, dengan kualitas yang sama atau bahkan lebih baik. Namun, karena H.266/VVC masih baru, belum semua perangkat mendukung format ini dan masih dalam tahap pengembangan.

--- main.py
+++ main.py
@@ -20,17 +20,17 @@ def main():
         # Pemeriksaan pilihan bahasa pengguna
         if lang_choice == '1':
             clear()
-            print("Program Konversi Video H.264 ke H.265\n")
+            print("Program Konversi Video H.264 ke H.266\n")
             input_text = "Masukkan nama file input (H.264): "
-            output_text = "Masukkan nama file output (H.265): "
+            output_text = "Masukkan nama file output (H.266): "
             done_text = "\nKonversi video selesai!"
             exit_text = "Tekan ENTER untuk keluar."
             break
         elif lang_choice == '2':
             clear()
-            print("H.264 to H.265 Video Conversion Program\n")
+            print("H.264 to H.266 Video Conversion Program\n")
             input_text = "Enter input file name (H.264): "
-            output_text = "Enter output file name (H.265): "
+            output_text = "Enter output file name (H.266): "
             done_text = "\nVideo conversion completed!"
             exit_text = "Press ENTER to exit."
             break
@@ -46,8 +46,8 @@ def main():
     # Membuat objek konversi menggunakan ffmpeg
     stream = ffmpeg.input(input_file)
 
-    # Mengkonversi video menggunakan format H.265
-    stream = ffmpeg.output(stream, output_file, vcodec='libx265')
+    # Mengkonversi video menggunakan format H.266 (AV1)
+    stream = ffmpeg.output(stream, output_file, vcodec='libsvtav1')
 
     # Menjalankan konversi video
     ffmpeg.run(stream)
